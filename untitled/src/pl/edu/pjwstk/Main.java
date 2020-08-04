8854ad5d171adcc41a7691679ccd26bd012bf277
Domds1998 <dominik.suchner@gmail.com>
2020-08-04 17:00:56 +0200

    Init - Reading from file is finished

---
+++ untitled/src/pl/edu/pjwstk/Main.java
@@ -0,0 +1,10 @@
+package pl.edu.pjwstk;
+
+public class Main {
+
+    public static void main(String[] args) {
+        for (int pocz = 0; pocz<10; ++pocz){
+            System.out.println(pocz);
+        }
+    }
+}
---
+++ untitled1/src/Main.java
@@ -0,0 +1,23 @@
+class Bird {
+
+    public Bird(){
+        sayName();
+    }
+
+    public void sayName(){
+        System.out.println("Bird");
+    }
+}
+class Pegas extends Bird {
+
+    @Override
+    public void sayName(){
+        System.out.println("Pegas");
+    }
+}
+public class Main {
+
+    public static void main(String[] args) {
+        Bird pegas = new Pegas();
+    }
+}
